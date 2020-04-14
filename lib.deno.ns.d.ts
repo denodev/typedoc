8ac2e6c4dc4261e6119d3094f1279b60a2d17c89
迷渡 <justjavac@gmail.com>
2020-04-15 04:40:34 +0800

    fix typo

--- lib.deno.ns.d.ts
+++ lib.deno.ns.d.ts
@@ -1435,7 +1435,7 @@ declare namespace Deno {
   export function readdir(path: string): Promise<FileInfo[]>;
 
   /** 采用同步方式将一个文件的内容和权限复制到另一个指定的路径，默认情况下根据需要
-   * 创建或者覆盖一个新文件。 如果目标路径是目录或不可写，则失败。
+   * 创建新文件或者覆盖原文件。 如果目标路径是目录或不可写，则失败。
    *
    *       Deno.copyFileSync("from.txt", "to.txt");
    *
