c2bdf50d2eed8a42e937642f67d1e7a85cc839dc
wangzy <sl2782087@163.com>
2020-04-15 20:41:44 +0800

    翻译：Deno.connect (#52)

--- lib.deno.ns.d.ts
+++ lib.deno.ns.d.ts
@@ -1967,8 +1967,7 @@ declare namespace Deno {
   }
 
   /**
-   * Connects to the hostname (default is "127.0.0.1") and port on the named
-   * transport (default is "tcp"), and resolves to the connection (`Conn`).
+   * 通过指定传输协议（默认 "tcp"）连接主机名（默认 "127.0.0.1"）和端口号，并异步返回这个连接（`Conn`）。
    *
    *     const conn1 = await Deno.connect({ port: 80 });
    *     const conn2 = await Deno.connect({ hostname: "192.0.2.1", port: 80 });
@@ -1976,7 +1975,8 @@ declare namespace Deno {
    *     const conn4 = await Deno.connect({ hostname: "golang.org", port: 80, transport: "tcp" });
    *     const conn5 = await Deno.connect({ address: "/foo/bar.sock", transport: "unix" });
    *
-   * Requires `allow-net` permission for "tcp" and `allow-read` for unix. */
+   * "tcp" 需要 `allow-net` 权限，unix 需要 `allow-read` 权限。
+   * */
   export function connect(
     options: ConnectOptions | UnixConnectOptions
   ): Promise<Conn>;
