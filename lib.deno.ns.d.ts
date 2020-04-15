7b9884d6a384d56c50de70346215299f67d02802
迷渡 <justjavac@gmail.com>
2020-04-15 08:51:01 +0800

    中英文之间添加空格

--- lib.deno.ns.d.ts
+++ lib.deno.ns.d.ts
@@ -1549,7 +1549,7 @@ declare namespace Deno {
    *
    * `type` 参数可以设置为 `dir` 或 `file`。此参数仅在 Windows 上可用，其他平台会被忽略。
    *
-   * 注意：此函数尚未在Windows上实现。
+   * 注意：此函数尚未在 Windows 上实现。
    *
    *       await Deno.symlink("old/name", "new/name");
    *
